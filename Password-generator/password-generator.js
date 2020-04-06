c9995daecd5e771b2f8beaa304fcee06f4a68ef1
Lusi Wang <lusiwangyk@gmail.com>
2020-04-06 19:09:09 -0400

    update password generator files

--- Password-generator/password-generator.js
+++ Password-generator/password-generator.js
@@ -1,20 +1,24 @@
-var slider = document.getElementById("slider");
+/*var slider = document.getElementById("slider");
 var lengthNum = document.getElementById("slider").value;
 
 slider.outinput = function (){
     document.getElementById("display").innerHTML = this.value; 
 }
-
+*/
+function myLength() {
+    var passwordLength = document.getElementById("length").value;
+    console.log(passwordLength);
+}
 
 
 function generatePassword() {
     var everything = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
     var password = "";
 
-    for (var i = 0; i <= lengthNum; i++) {
+    for (var i = 0; i <= 12; i++) {
         password = password + everything.charAt(Math.floor(Math.random() * Math.floor(everything.length-1)));
     };
-    document.getElementById("display").value = password;
+    document.getElementById("display") = password;
     console.log(password);
 }
 
